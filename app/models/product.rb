94e1c37229542925473dbcc024a38676d6a8edbc
EC2 Default User <ec2-user@ip-172-31-29-4.ap-northeast-1.compute.internal>
2021-02-01 22:27:45 +0000

    create new product

--- app/models/product.rb
+++ app/models/product.rb
@@ -28,7 +28,8 @@ class Product < ApplicationRecord
   }
   
   scope :in_cart_product_names, -> (cart_item_ids) { where(id: cart_item_ids).pluck(:name) }
-  
+  scope :recently_products, -> (number) { order(created_at: "desc").take(number) }
+
   def reviews_new
     reviews.new
   end
