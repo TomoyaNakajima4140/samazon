51ee75f4eac5310cd38d9c27d6d92d8cb3670721
EC2 Default User <ec2-user@ip-172-31-29-4.ap-northeast-1.compute.internal>
2021-02-03 07:59:05 +0000

    create featured_products

--- app/controllers/dashboard/products_controller.rb
+++ app/controllers/dashboard/products_controller.rb
@@ -60,7 +60,7 @@ class Dashboard::ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:name, :description, :price, :category_id)
+      params.require(:product).permit(:name, :description, :price, :recommend_flag, :category_id)
     end
   
 end
